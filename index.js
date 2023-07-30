3a02542258b5349a3e1ee8fb3914356aaf730c30
oluwatoyin oredein <teboola2@yahoo.com>
2023-07-30 05:52:49 +0100

    upadate files

--- index.js
+++ index.js
@@ -7,24 +7,40 @@ const port = 3000;
 const date = new Date();
 const year = date.getFullYear();
 
+const Tasks = ["Do chores", "Buy milk"];
+const Works = ["Stand up meeting", "Documentation"];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+app.post('/submit', (req, res) => {
+    let newTask = req.body.newtask;
+    Tasks.push(newTask);
+    res.redirect('/');
+});
+app.post('/submit/work', (req, res) => {
+    let newWork = req.body.newwork;
+    Works.push(newWork);
+    res.redirect('/work');
+});
+app.post('/strike', (req, res) => {
+    const checked = req.body.check;
+    console.log(req.body)
+})
 app.get('/', (req, res) => {
 
     const todayDate = date.getDate();
     const todayDay = date.getDay();
     const month = date.getMonth();
     const today = days[todayDay] + ", " + months[month] + " " + todayDate;
-    res.render('index.ejs', { Today: today, Year: year });
-})
-app.get('/work', (req, res) => {
-    res.render('work.ejs');
-})
 
-app.post('/', (req, res) => {
-
+    res.render('index.ejs', { Today: today, Year: year, task: Tasks });
 });
+app.get('/work', (req, res) => {
+    res.render('work.ejs', { Year: year, worked: Works });
+});
+
 
 
 
@@ -35,4 +51,4 @@ app.listen(port, () => {
 
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
