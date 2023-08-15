4fd8ece14014c19e2caaa3df6e1995322960dd80
oluwatoyin oredein <teboola2@yahoo.com>
2023-08-15 15:16:33 +0100

    update files

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 ### Todoey is a todo web app that allows user create new todos and strike out completed todo.
 ---
 - This project is to solidfy my learning of Nodejs.
+- Use mongoDB to manipulate data input.
 - Frontend and Backend done by Oluwatoyin.
 ---
-- User can creat a new todo and strike out completed todos on home page.
+- User can create a new todo and also delete completed todos
 ---
 ![Todoey Home Page](./public/img/Todey%20Screenshot.png)
 ---
-Left with handling the work page strike out and deleting completed task on both page.
--- index.js
+++ index.js
@@ -1,51 +1,135 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
 
+//Connect to mongoose
+mongoose.connect("mongodb://127.0.0.1:27017/todoeyDB");
+
 const date = new Date();
 const year = date.getFullYear();
 
-const Tasks = ["Do chores", "Buy milk"];
-const Works = ["Stand up meeting", "Documentation"];
+//Setup mongoose schema
+const tasksSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+})
+const Tasks = mongoose.model("task", tasksSchema);
+const chores = new Tasks({
+    name: "Do chores"
 
+});
+const buy = new Tasks({
+    name: "Buy Milk"
+});
+const defaultArray = [chores, buy];
+
+//Use bodyparser to get post request from frontend and 
+//express to render static files
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Render data to Frontend
+app.get('/', async (req, res) => {
 
-app.post('/submit', (req, res) => {
-    let newTask = req.body.newtask;
-    Tasks.push(newTask);
-    res.redirect('/');
-});
-app.post('/submit/work', (req, res) => {
-    let newWork = req.body.newwork;
-    Works.push(newWork);
-    res.redirect('/work');
-});
+    try {
+        const todayDate = date.getDate();
+        const todayDay = date.getDay();
+        const month = date.getMonth();
+        const today = days[todayDay] + ", " + months[month] + " " + todayDate;
 
+        const task = await Tasks.find({});
+        if (task.length === 0) {
+            Tasks.insertMany(defaultArray);
+            res.redirect('/');
+        } else {
+            res.render('index.ejs', { Today: today, Year: year, task: task });
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 
-app.get('/', (req, res) => {
-
-    const todayDate = date.getDate();
-    const todayDay = date.getDay();
-    const month = date.getMonth();
-    const today = days[todayDay] + ", " + months[month] + " " + todayDate;
-
-    res.render('index.ejs', { Today: today, Year: year, task: Tasks });
-});
-app.get('/work', (req, res) => {
-    res.render('work.ejs', { Year: year, worked: Works });
 });
 
+//Receive request from frontend and push to mongoose
+app.post('/submit', async (req, res) => {
+    try {
+        const newTask = req.body.newtask;
+        const newTaskName = new Tasks({
+            name: newTask
+        });
+        await newTaskName.save();
+        res.redirect('/');
+    }
+    catch (err) {
+        console.log(err);
+    }
 
-
-
-
+});
+app.post('/delete', async (req, res) => {
+    try {
+        const checkedItemId = req.body.checkbox;
+        await Tasks.findByIdAndRemove(checkedItemId);
+        res.redirect('/');
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+//Listening 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
+
+//mongoose.connection.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
